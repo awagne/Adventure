be61f34e089c7920a865932b2585927328e0df00
Alan Reed Wagner <wagneralan95@gmail.com>
2023-06-12 16:15:37 -0400

    Create Fire item

--- src/Fire.java
+++ src/Fire.java
@@ -1,5 +1,11 @@
 package src;
 
 public class Fire extends Item{
-    
+    public Fire(){
+        this.setName("Fire");
+    }
+
+    public void useItem(Player player){
+        player.setStatus("Fire");
+    }
 }
