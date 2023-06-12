9013247ca6a037d3a5280cd8884ff32e9f9582c2
Alan Reed Wagner <wagneralan95@gmail.com>
2023-06-12 16:15:59 -0400

    Created Potion

--- src/Potion.java
+++ src/Potion.java
@@ -1,5 +1,12 @@
 package src;
 
-public class Potion extends Item{
-    
+public class Potion extends Item {
+
+    public Potion(){
+        this.setName("Potion");
+    }
+
+    public void useItem(Player player){
+        player.setHealth((int)(player.getHealth() * 1.25));
+    }
 }
