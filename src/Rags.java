823db3975b2ff46f06a509de2317da6dde25c70c
Alan Reed Wagner <wagneralan95@gmail.com>
2023-06-12 16:56:09 -0400

    Added Rags Armor

--- src/Rags.java
+++ src/Rags.java
@@ -1,5 +1,13 @@
 package src;
 
-public class Rags extends Armour{
+public class Rags extends Armor{
+
+    public Rags(){
+        this.setType("Rags");
+    }
     
+    public void Hit(Player player, int value){
+        this.setDurability(50);
+        player.setHealth(player.getHealth() - (value - 2));
+    }
 }
