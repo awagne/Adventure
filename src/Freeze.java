9095c86de0abbc40fa4b90e91ee08f4e9c4180be
Alan Reed Wagner <wagneralan95@gmail.com>
2023-06-12 16:15:48 -0400

    Created Freeze item

--- src/Freeze.java
+++ src/Freeze.java
@@ -1,5 +1,11 @@
 package src;
 
 public class Freeze extends Item{
-    
+    public Freeze(){
+        this.setName("Freeze");
+    }
+
+    public void useItem(Player player){
+        player.setStatus("Freeze");
+    }
 }
