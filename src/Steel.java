3042e6dfcd500410cce2366c677325c97e092f0c
Alan Reed Wagner <wagneralan95@gmail.com>
2023-06-12 16:55:59 -0400

    Added Steel Armor

--- src/Steel.java
+++ src/Steel.java
@@ -1,5 +1,13 @@
 package src;
 
-public class Steel extends Armour{
+public class Steel extends Armor{
     
+    public Steel(){
+        this.setType("Steel");
+    }
+
+    public void Hit(Player player, int value){
+        this.setDurability(20);
+        player.setHealth(player.getHealth() - (value - 10));
+    }
 }
