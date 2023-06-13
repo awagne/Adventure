4332ba8b5728f1630fa0335dc3541f341ab923c5
Alan Reed Wagner <wagneralan95@gmail.com>
2023-06-13 13:14:47 -0400

    Added Enemy functions

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,49 @@
 package src;
 
 public class Enemy {
-    
+
+    boolean burning = false;
+    boolean frozen = false;
+    int health = 100;
+    int attack = 1;
+
+    public Enemy(){
+        
+    }
+
+    public void Hit(int damage, String status){
+        if(status == "Freeze"){
+            frozen = true;
+        }
+        else if(status == "Fire"){
+            burning = true;
+        }
+        
+        health -= damage;
+
+    }
+
+    public void Attack(Player player){
+        if(burning){
+            health -= 2;
+        }
+        
+        player.getHit(attack);
+    }
+
+    public void setAttack(int val){
+        attack = val;
+    }
+
+    public int getAttack(){
+        return attack;
+    }
+
+    public void setHealth(int val){
+        health = val;
+    }
+
+    public int getHealth(){
+        return health;
+    }
 }
