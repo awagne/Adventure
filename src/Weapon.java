7225396cfb764145898d18c3ef5a2163eb856011
Alan Reed Wagner <wagneralan95@gmail.com>
2023-06-13 12:46:25 -0400

    Added weapons

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,5 +1,34 @@
 package src;
 
 public class Weapon {
+
+    private String type = "";
+    private int attack = 0;
     
+    public Weapon(){
+
+    }
+
+    public void SetType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setAttack(Player player, int val, double percent){
+        if((int)(player.getAttack() * percent) > val){
+            attack = (int)(player.getAttack() * percent);
+        }
+        else{
+            attack = val;
+        }
+    }
+
+    public Integer getAttack(){
+        return attack;
+    }
+
+
 }
